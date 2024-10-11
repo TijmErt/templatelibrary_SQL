87471fa42bed07c42b4eef8186d5e12a43f92c25
Tijm van Dongen <i486333@Fhict.nl>
2024-10-11 12:37:08 +0200

    initial commit

---
+++ src/main/java/com/sjitzooi/templatelibrary_sql/TemplatelibrarySqlApplication.java
@@ -0,0 +1,13 @@
+package com.sjitzooi.templatelibrary_sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplatelibrarySqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplatelibrarySqlApplication.class, args);
+    }
+
+}
